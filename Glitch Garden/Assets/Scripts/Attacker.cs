9d7028aef3625d5ecc38467ca5a1f1c3e615d020
Dmitry <dmitrykonyshov@gmail.com>
2021-01-01 12:38:13 +0200

    Spawn Attackers Using Coroutine

--- Glitch Garden/Assets/Scripts/Attacker.cs
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -4,12 +4,6 @@ public class Attacker : MonoBehaviour
 {
     [Range(0f, 5f)][SerializeField] float walkSpeed = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
