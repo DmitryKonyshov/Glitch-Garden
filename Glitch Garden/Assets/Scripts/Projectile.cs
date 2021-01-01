112f8c3d25a4c3b6c479be83dafd023f86f01e26
Dmitry <dmitrykonyshov@gmail.com>
2021-01-01 17:52:19 +0200

    Make Projectile Move

---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
