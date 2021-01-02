6cbd863d3e3d537055e622677cd89ef341e0350b
Dmitry <dmitrykonyshov@gmail.com>
2021-01-02 09:47:24 +0200

    Click To Spawn Defenders

---
+++ Glitch Garden/Assets/Scripts/DefendersSpawner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class DefendersSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject defender;
+
+    private void OnMouseDown()
+    {
+        SpawnDefender();
+    }
+
+    private void SpawnDefender()
+    {
+        GameObject newDefender = Instantiate(defender, transform.position, Quaternion.identity) as GameObject;
+    }
+}
