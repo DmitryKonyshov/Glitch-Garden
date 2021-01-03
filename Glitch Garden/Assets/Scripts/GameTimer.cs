704b88e80879a4f739c006b3c933633ce65ab720
Dmitry <dmitrykonyshov@gmail.com>
2021-01-03 15:41:15 +0200

    Create A Level Timer

---
+++ Glitch Garden/Assets/Scripts/GameTimer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+    [Tooltip("Our level timer in SECONDS")]
+    [SerializeField] float levelTime = 10f;
+    
+    void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= levelTime);
+
+        if (timerFinished)
+        {
+            Debug.Log("level timer expired!");
+        }
+    }
+}
