74e6abed5023ef117fbfbe7c674572aa9fc77a18
Dmitry <dmitrykonyshov@gmail.com>
2021-01-04 17:28:52 +0200

    Set & Save Music Volume

---
+++ Glitch Garden/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void SetVolume(float volume)
+    {
+        _audioSource.volume = volume;
+    }
+}
